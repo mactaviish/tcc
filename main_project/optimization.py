e976722803b9056691feba2587b8ec30dc122fce
Leonardo Dalla Corte <37817455+mactaviish@users.noreply.github.com>
2025-04-03 21:56:49 -0300

    add print solution

--- main_project/optimization.py
+++ main_project/optimization.py
@@ -1,4 +1,5 @@
 import gurobipy as gp
+import sys
 from gurobipy import GRB, Model
 from typing import List
 from models.airplane import Airplane
@@ -20,10 +21,10 @@ def run_optimization(airplanes: List[Airplane], routes: List[Route]):
   model.setParam(GRB.Param.TimeLimit, 600)
   model.optimize()
 
-  print_solution(model)
+  print_solution(model, airplane_flow, passenger_count, binary, binary2)
 
 def create_variables(model: Model, airplanes: List[Airplane], routes: List[Route]):
-  T, C, F, P, BIN, BIN2, K, Z = {}, {}, {}, {}, {}, {}, 7, 100
+  T, C, F, P, BIN, BIN2, K, Z = {}, {}, {}, {}, {}, {}, 2, sys.maxsize
 
   for route in routes:
     for airplane in airplanes:
@@ -69,6 +70,8 @@ def add_constraints(model: Model, airplanes: List[Airplane], routes: List[Route]
       model.addConstr(F[key] <= Z * BIN[key], name=f"link_F_BIN_{key}")
       model.addConstr(F[key] <= Z * BIN2[airplane.model], name=f"link_F_BIN2_{key}")
       model.addConstr(P[key] <= airplane.seats * F[key], name=f"capacity_{key}")
+      model.addConstr(P[key] >= 0, name=f"non_negativity_passenger_{key}")
+      model.addConstr(F[key] >= 0, name=f"non_negativity_fleet_flow_{key}")
 
 # (3.6, 3.9)
   for route in routes:
@@ -78,9 +81,16 @@ def add_constraints(model: Model, airplanes: List[Airplane], routes: List[Route]
 # (3.8)
   model.addConstr(gp.quicksum(BIN2[airplane.model] for airplane in airplanes) <= K, name=f"aircraft_types")
 
-def print_solution(model: Model):
-  # model.write("./output/solution.sol")
-  model.write('./output/model.mps')
-  model.write('./output/model.lp')
-  model.computeIIS()
-  model.write("./output/model.ilp")
+def print_solution(model: Model, F, P, BIN, BIN2):
+  if model.status == gp.GRB.OPTIMAL:
+    print(f"\nValor objetivo: {model.ObjVal}")
+    print("\n--- SOLUÇÃO ENCONTRADA ---")
+    for key, var in F.items():
+        if var.X > 0:
+            print(f"Destino: {key[0]} | Aeronave: {key[1]}")
+            print(f"  Aeronaves: {F[key].X}")
+            print(f"  Passageiros: {P[key].X}")
+            print(f"  BIN (rota ativa): {BIN[key].X}")
+            print(f"  BIN2 (modelo usado): {BIN2[key[1]].X}\n")
+  else:
+      print("⚠️ Nenhuma solução ótima foi encontrada.")
